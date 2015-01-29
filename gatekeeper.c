38a7f4713a6dc827f8e4ecb097775d91aba89fa5
Steven Vittitoe <bool101@gmail.com>
2015-01-29 15:20:59 -0600

    added logging off stdio, added alarm call in parent, sigchild handler added

--- gatekeeper.c
+++ gatekeeper.c
@@ -22,14 +22,19 @@
 #include <net/if.h>
 #include <ctype.h>
 
+/* Defines */
 #define FAILURE -1
 #define SUCCESS 1
 
+/* Globals */
 int log_fd;
+struct sockaddr_in log_addr;
 
+/* Prototypes */
 void usage(void);
 void Log(char *format, ...);
 int main(int argc, char * argv[]);
+void sigchld(int sig);
 int setup_logsocket(char * logsrvstr);
 int setup_listener(char * listenstr, int * out_fd_r, int * out_fd_w);
 int parse_address_string(char * instr, void * addr, size_t addr_size, unsigned short * port, int address_family);
@@ -39,13 +44,37 @@ int init_tcp6(unsigned short port, struct in6_addr * ia6);
 int init_udp4(unsigned short port, struct in_addr * ia4);
 int init_udp6(unsigned short port, struct in6_addr * ia6);
 
+/*
+ * Log functions takes in a format string and variable arguments.
+ * It sends the resulting printf off to a UDP socket stored in log_buf
+ * to a host/port specified in log_addr.
+ *
+ * We need to do this because writing to stdout/err will mess with with clients
+ * that use these FD's. Basically, no printing. K?
+ *
+ * Will break this rule ONLY if log_fd == -1.
+ */
+
 void Log(char *format, ...)
 {
     va_list ap;
+    char log_buf[1024];
     va_start(ap, format);
-    (void) vfprintf(stderr, format, ap);
+    vsnprintf(log_buf, sizeof(log_buf)-1, format, ap);
+    if (log_fd == -1)
+    {
+    	fprintf(stderr, log_buf);
+    }
+    else
+    {
+    	sendto(log_fd, log_buf, strlen(log_buf), 0, (struct sockaddr*)&log_addr, sizeof(log_addr));
+    }
     va_end(ap);
 }
+
+/*
+ * Logs usage and returns. Does not exit.
+ */
 
 void usage(void)
 {
@@ -53,11 +82,27 @@ void usage(void)
     Log("      -l <listen string> \n");
     Log("      -r <redirect string> \n");
     Log("      -o <optional log server string> \n");
+    Log("      -c <optional capture host string> \n");
+    Log("      -a <optional alarm in seconds> \n");
     Log("      -k <optional path to key file>\n\n");
     Log("   example usage: gatekeeper -l stdio -r stdio:/home/atmail/atmail -k /home/keys/atmail -l 10.0.0.2:2090\n");
-    Log("                  gatekeeper -l tcpipv4:0.0.0.0:1234 -r stdio:/home/atmail/atmail\n\n");
+    Log("                  gatekeeper -l tcpipv4:0.0.0.0:1234 -r stdio:/home/atmail/atmail -a 5\n");
     Log("                  gatekeeper -l tcpipv6:[::]:1234 -r stdio:/home/atmail/atmail\n\n");
 }
+
+/*
+ * Entry point. Performs argument parsing and validation. Sets up log socket, listen socket, and enters
+ * main client service loop.
+ *
+ * This program is designed to listen on one of stdio/tcp/udp and redirect to one of stdio/tcp/udp on ipv4 and ipv6.
+ * Mix and match as required. These are listenstr and redirectstr arguments/variables.
+ *
+ * Specifying a log server is necessary for programs listening on stdio.
+ * Specifying a capture host will send a copy of all traffic sent or received to the capture server.
+ * Specifying an alarm value will set and alarm for that number of seconds
+ * Specifying a key file will setup inotify monitoring on that key file.
+ *
+ */
 
 int main(int argc, char * argv[])
 {
@@ -65,77 +110,146 @@ int main(int argc, char * argv[])
     char * listenstr   = NULL;
     char * redirectstr = NULL;
     char * logsrvstr   = NULL;
-    int32_t c          = -1;
+    char * capsrvstr   = NULL;
+    int c              = -1;
+    unsigned int alarmval = 0;
     int listen_fd_r    = -1;
     int listen_fd_w    = -1;
     int remote_fd_r    = -1;
     int remote_fd_w    = -1;
-    log_fd             = -1;
 
+    /* initialize globals */
+    log_fd             = -1;
+    memset(&log_addr, 0, sizeof(log_addr));
+
+    /* at least 5 arguments to run: program, -l, listenstr, -r, redirstr */
     if (argc < 5)
     {
         usage();
-        exit(1);
+        goto cleanup;
     }
-    while ((c=getopt(argc, argv, "hl:r:k:o:")) != -1)
+
+    /* avoid zombie children */
+    if (signal(SIGCHLD, sigchld) == SIG_ERR)
+    {
+        Log("Unable to set SIGCHLD handler");
+        goto cleanup;
+    }
+
+    while ((c=getopt(argc, argv, "hl:r:k:o:c:a:")) != -1)
     {
         switch (c)
         {
         case 'h':
+        	/* help and exit*/
             usage();
-            return SUCCESS;
+            goto cleanup;
         case 'l':
+        	/* save off listen argument string -- required*/
             listenstr = optarg;
             break;
         case 'r':
+        	/* save off redirect argument string -- required*/
             redirectstr = optarg;
             break;
         case 'k':
+        	/* save off keyfile argument (optional) */
             keyfile = optarg;
             break;
         case 'o':
             logsrvstr = optarg;
             break;
+        case 'c':
+            capsrvstr = optarg;
+            break;
+        case 'a':
+            alarmval = atoi(optarg);
+            break;
         case '?':
             usage();
-            if ('l' == optopt || 'r' == optopt || 'k' == optopt || 'o' == optopt)
+            if ('l' == optopt || 'r' == optopt || 'k' == optopt || 'o' == optopt || 'c' == optopt || 'a' == optopt)
                 Log("Option '-%c' requires an argument\n", optopt);
             else if(isprint(optopt))
                 Log("Unknown options '-%c'\n", optopt);
             else
                 Log("Unknown getopt return: 0x%x\n", c);
-            return SUCCESS;
+            goto cleanup;
         }
     }
 
+    /* check for required arguments */
+    if (listenstr == NULL)
+    {
+    	Log("Argument -l is required\n");
+    	goto cleanup;
+    }
+
+    if (redirectstr == NULL)
+    {
+    	Log("Argument -r is required\n");
+    	goto cleanup;
+    }
+
+    /* setup logging server globals */
     if (logsrvstr != NULL)
     {
         if (setup_logsocket(logsrvstr) == FAILURE)
         {
-            goto failure;
+        	Log("Failed to setup log server socket\n");
+            goto cleanup;
         }
     }
 
-    if (setup_listener(listenstr, &listen_fd_r, &listen_fd_w) == FAILURE)
+    /* set alarm is one was specified */
+    if (alarmval != 0)
     {
-        goto failure;
+    	alarm(alarmval);
     }
 
-    return SUCCESS;
-failure:
+    /* setup listener fd's */
+    if (setup_listener(listenstr, &listen_fd_r, &listen_fd_w) == FAILURE)
+    {
+        goto cleanup;
+    }
+
+    /* setup redirect fd's */
+
+    /* start the pumps */
+
+
+cleanup:
     if (listen_fd_r != -1)
         close(listen_fd_r);
     if (listen_fd_w != -1)
         close(listen_fd_w);
     if (log_fd != -1)
         close(log_fd);
-    return FAILURE; 
+
+    return SUCCESS;
+}
+
+/*
+ * This farms exit status from forked children to avoid
+ * having any zombie processes lying around
+ */
+
+void sigchld(int sig) {
+    int status;
+    while (wait4(-1, &status, WNOHANG, NULL) > 0) {  }
 }
 
 int setup_logsocket(char * logsrvstr)
 {
-
+	log_addr.sin_family = AF_INET;
+	if(parse_address_string(logsrvstr, &log_addr.sin_addr.s_addr, sizeof(log_addr.sin_addr.s_addr), &log_addr.sin_port, AF_INET) == SUCCESS)
+	{
+		log_fd = socket(AF_INET, SOCK_DGRAM, 0);
+		if (log_fd == -1)
+			return FAILURE;
+	}
+	return SUCCESS;
 }
+
 int setup_listener(char * listenstr, int * out_fd_r, int * out_fd_w)
 {
     struct in_addr addr;
