6d93dc19af02dfb14b4e59f61785fb26bcfc4d2e
Zach Riggle <riggle@google.com>
2015-06-09 18:28:36 -0400

    Add setpgid

---
+++ setpgid/setpgid.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <linux/sched.h>
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Usage: %s <pgid> argv0 argv1\n", argv[0]);
+        exit(1);
+    }
+    setpgid(0, atoi(argv[1]));
+    execvp(argv[2], &argv[2]);
+}
