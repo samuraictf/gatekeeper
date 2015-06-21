435ea9314cff4e8f63bf898663422c3e92da5c5f
Zach Riggle <riggle@google.com>
2015-06-21 17:01:43 -0400

    Extract malloc functionality

---
+++ malloc/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <linux/sched.h>
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Usage: %s <dir> argv0 argv1\n", argv[0]);
+        exit(1);
+    }
+
+    set_malloc_flags();
+
+    execvp(argv[1], &argv[1]);
+}
--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4,14 +4,7 @@
 #include <sched.h>
 #include <linux/sched.h>
 
-int main(int argc, char** argv) {
-    if(argc < 2) {
-        printf("Usage: %s <dir> argv0 argv1\n", argv[0]);
-        exit(1);
-    }
-
+void set_malloc_flags() {
     setenv("MALLOC_PERTURB_", "127", 1);
     setenv("MALLOC_CHECK_", "2", 1);
-
-    execvp(argv[1], &argv[1]);
 }
