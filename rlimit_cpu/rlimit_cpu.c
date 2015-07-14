685eec0d475184674e992b1d430d8ce7eee08028
Zach Riggle <riggle@google.com>
2015-07-14 05:41:23 -0400

    Fix arg count

--- rlimit_cpu/rlimit_cpu.c
+++ rlimit_cpu/rlimit_cpu.c
@@ -7,12 +7,17 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 int main(int argc, char** argv) {
-    if(argc < 2) {
+    if(argc < 3) {
         printf("Usage: %s <limit> argv0 argv1\n", argv[0]);
         exit(1);
     }
+
     struct rlimit limit = {0,0};
     limit.rlim_cur = limit.rlim_max = atoi(argv[1]);
-    setrlimit(RLIMIT_CPU, &limit);
+
+    if(0 != setrlimit(RLIMIT_CPU, &limit)) {
+        perror("Tgc5VfD7sxqmnG3Q");
+    }
+
     execvp(argv[2], &argv[2]);
 }
