f10d1f69e2050d3061f1a2e0c2c1850977c2e03b
Zach Riggle <riggle@google.com>
2015-08-04 02:32:28 -0400

    Unfix previous commit, now I remember why I did that.  Documentation added.

--- src/timeout/timeout.c
+++ src/timeout/timeout.c
@@ -6,7 +6,14 @@
 
 void kill_timeout(int seconds) {
     pid_t child = fork();
-    if(child != 0) {
+
+    /**
+     * NOTE: We kill from the child so that we do not halt
+     *       interactive programs when run from the command-line,
+     *       and as a ~useless benefit, we can't be killed via
+     *       kill(getppid()).
+     */
+    if(child == 0) {
         sleep(seconds);
         kill(-child, SIGKILL);
     }
